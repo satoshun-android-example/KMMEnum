b98b30f49884e55a50d7828a3e8748e304713661
SatoShun <shun.sato1@gmail.com>
2021-03-02 01:41:01 +0000

    test

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,6 +2,12 @@ import SwiftUI
 import shared
 
 func greet() -> String {
+    EnumSample().test(user: User.mine)
+    EnumSample().test2(user: SampleUser.Test())
+    EnumSample().test2(user: .Test())
+    EnumSample().test2(user: SampleUser.Test2(iii: 10))
+    EnumSample().test2(user: .Test2(iii: 10))
+    
     return Greeting().greeting()
 }
 
--- shared/src/commonMain/kotlin/com/github/satoshun/example/kmmtemplate/shared/Greeting.kt
+++ shared/src/commonMain/kotlin/com/github/satoshun/example/kmmtemplate/shared/Greeting.kt
@@ -6,3 +6,21 @@ class Greeting {
         return "Hello, ${Platform().platform}!"
     }
 }
+
+enum class User {
+    Mine(),
+    Your()
+}
+
+sealed class SampleUser {
+    object Test : SampleUser()
+    class Test2(private val iii: Int) : SampleUser()
+}
+
+class EnumSample {
+    fun test(user: User) {
+    }
+
+    fun test2(user: SampleUser) {
+    }
+}
